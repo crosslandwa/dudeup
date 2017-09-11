a8521115cd93751ef4682a22d20c1e2978ff6565
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-09-11 08:37:18 +0100

    Calculate total per person when no payments made

--- settleup.js
+++ settleup.js
@@ -1,3 +1,8 @@
-const SettleUp = () => 'hello from settleup'
+function SettleUp (people) {
+  return Object.keys(people).reduce((acc, name) => {
+    acc[name] = { totalPaid: 0 }
+    return acc
+  }, {})
+}
 
 module.exports = SettleUp
--- spec/settleupSpec.js
+++ spec/settleupSpec.js
@@ -1,7 +1,15 @@
 const SettleUp = require('../settleup')
 
 describe('Settle Up', () => {
-  it('says hello', () => {
-    expect(SettleUp()).toEqual('hello from settleup')
+  it('returns a total paid of zero when no purchases have been made', () => {
+    const noPayments = () => { paid: {} }
+    expect(SettleUp({ eAndG: noPayments(), sAndW: noPayments() })).toEqual({
+      eAndG: {
+        totalPaid: 0
+      },
+      sAndW: {
+        totalPaid: 0
+      }
+    })
   })
 })
