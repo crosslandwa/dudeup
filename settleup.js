ff91b8f9120b951797eecbdb3b666f5aadec3c93
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-09-11 08:43:32 +0100

    Total up payments per person

--- settleup.js
+++ settleup.js
@@ -1,6 +1,7 @@
 function SettleUp (people) {
   return Object.keys(people).reduce((acc, name) => {
-    acc[name] = { totalPaid: 0 }
+    const person = people[name]
+    acc[name] = { totalPaid: Object.keys(person.paid).map(item => person.paid[item]).reduce((total, amount)  => total + amount, 0) }
     return acc
   }, {})
 }
--- spec/settleupSpec.js
+++ spec/settleupSpec.js
@@ -1,14 +1,31 @@
 const SettleUp = require('../settleup')
 
+const noPayments = () => ({ paid: {} })
+
 describe('Settle Up', () => {
   it('returns a total paid of zero when no purchases have been made', () => {
-    const noPayments = () => { paid: {} }
     expect(SettleUp({ eAndG: noPayments(), sAndW: noPayments() })).toEqual({
       eAndG: {
         totalPaid: 0
       },
       sAndW: {
         totalPaid: 0
+      }
+    })
+  })
+
+  it('returns a total paid per person when purchases have been made', () => {
+    const eAndG = noPayments()
+    eAndG.paid['a'] = 1
+    eAndG.paid['b'] = 2
+    const sAndW = noPayments()
+    sAndW.paid['c'] = 0.5
+    expect(SettleUp({ eAndG, sAndW })).toEqual({
+      eAndG: {
+        totalPaid: 3
+      },
+      sAndW: {
+        totalPaid: 0.5
       }
     })
   })
