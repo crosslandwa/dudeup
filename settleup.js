b1b17470b5b0896bd3395777aae70eb564be79e4
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-09-11 08:44:36 +0100

    Total up payments per person

--- settleup.js
+++ settleup.js
@@ -6,6 +6,8 @@ function SettleUp (people) {
   }, {})
 }
 
-const totalPaid = person => Object.keys(person.paid).map(item => person.paid[item]).reduce((total, amount)  => total + amount, 0)
+const totalPaid = person => Object.keys(person.paid)
+  .map(item => person.paid[item])
+  .reduce((total, amount)  => total + amount, 0)
 
 module.exports = SettleUp
