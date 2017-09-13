6449a7b9ccd611188bcf8f17e5f2585cb52e415a
will.crossland <crossland.w.a@gmail.com>
2017-09-13 22:48:22 +0100

    Remove react logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import NamesStrip from './NamesStrip'
 import GroupSummary from './GroupSummary'
@@ -11,7 +10,6 @@ class App extends Component {
     return (
       <div className="App" >
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Dude Up</h2>
         </div>
         <div className="App-group">
