755fd49d75f02309754400938955175823b736e6
will.crossland <crossland.w.a@gmail.com>
2017-09-18 14:02:10 +0100

    Scope settle up calculations by list

--- src/settleup/reducers.js
+++ src/settleup/reducers.js
@@ -16,16 +16,19 @@ export function settleUpReducer (state = initialState, action) {
 export function settleUpCalculationsReducer (state, action) {
   if (actionsThatDoNotAffectPrices.includes(action.type)) return state
 
-  const formatted = state.entities.dudes.allIds.reduce((acc, dudeId) => {
-    acc[dudeId] = state.entities.items.allIds
-      .map(itemId => state.entities.items.byId[itemId])
-      .filter(item => item.dudeId === dudeId)
-      .reduce((amountAcc, item) => {
-        amountAcc[item.id] = item.price
-        return amountAcc
-      }, {})
-    return acc
-  }, {})
+  if (!state.selectedListId) return state
+
+  const formatted = state.entities.lists.byId[state.selectedListId].dudeIds
+    .reduce((acc, dudeId) => {
+      acc[dudeId] = state.entities.items.allIds
+        .map(itemId => state.entities.items.byId[itemId])
+        .filter(item => item.dudeId === dudeId)
+        .reduce((amountAcc, item) => {
+          amountAcc[item.id] = item.price
+          return amountAcc
+        }, {})
+      return acc
+    }, {})
 
   const updated = clone(state)
   updated.settleUp = SettleUp(formatted)
