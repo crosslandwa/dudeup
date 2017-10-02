0778d874d68126207019de2d2374f2e0666fab41
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-10-02 23:34:54 +0100

    Refactor out owedByEachMember

--- dudeup.js
+++ dudeup.js
@@ -12,17 +12,22 @@ function DudeUp (groupMemberPayments) {
   const totalPaidPerGroupMember = mapEachGroupMember(name => sum(groupMemberPayments[name]))
   const groupTotal = sum(entries(totalPaidPerGroupMember).map(([name, total]) => total))
   const averageAmountPerGroupMember = rounded(groupTotal / Math.max(1, groupMembers.length))
+  const amountOwedByGroupMember = owedByEachMember(mapEachGroupMember, totalPaidPerGroupMember, averageAmountPerGroupMember)
 
+  return { averageAmountPerGroupMember, groupTotal, amountOwedByGroupMember, totalPaidPerGroupMember }
+}
+
+function owedByEachMember (mapEachGroupMember, totalPaidPerGroupMember, averageAmountPerGroupMember) {
   const balances = mapEachGroupMember(name => rounded(totalPaidPerGroupMember[name] - averageAmountPerGroupMember))
   const stillOwed = () => entries(balances).filter(([name, balance]) => balance > 0)
   const stillOwing = () => entries(balances).filter(([name, balance]) => balance < 0)
-  const amountOwedByGroupMember = mapEachGroupMember(name => ({}))
+  const result = mapEachGroupMember(name => ({}))
   while (stillOwed().length) {
     const [owedToName, owedToBalance] = stillOwed()[0]
     if (stillOwing().length) {
       const [owedByName, owedByBalance] = stillOwing()[0]
       const amount = Math.min(owedToBalance, Math.abs(owedByBalance))
-      amountOwedByGroupMember[owedByName][owedToName] = amount
+      result[owedByName][owedToName] = amount
       balances[owedToName] = rounded(owedToBalance - amount)
       balances[owedByName] = rounded(owedByBalance + amount)
     } else {
@@ -30,13 +35,7 @@ function DudeUp (groupMemberPayments) {
       balances[owedToName] = rounded(0)
     }
   }
-
-  return {
-    averageAmountPerGroupMember,
-    groupTotal,
-    amountOwedByGroupMember,
-    totalPaidPerGroupMember
-  }
+  return result
 }
 
 module.exports = DudeUp
