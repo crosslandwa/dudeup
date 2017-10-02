22bf8d1c27fc2aa8343b35461ee17d3a781f838b
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-10-02 23:09:17 +0100

    Refactor out pulling groupMembers from map keys

--- dudeup.js
+++ dudeup.js
@@ -3,22 +3,22 @@ const sum = values => values.reduce((total, x) => rounded(total + x), 0)
 const rounded = amount => Math.round(amount * 1e2) / 1e2
 
 function DudeUp (groupMemberPayments) {
-  const totalPaidPerGroupMember = Object.keys(groupMemberPayments).reduce((acc, name) => {
+  const groupMembers = Object.keys(groupMemberPayments)
+  const totalPaidPerGroupMember = groupMembers.reduce((acc, name) => {
     acc[name] = sum(groupMemberPayments[name])
     return acc
   }, {})
   const groupTotal = sum(entries(totalPaidPerGroupMember).map(([name, total]) => total))
-  const numberOfGroupMembers = Object.keys(groupMemberPayments).length
-  const averageAmountPerGroupMember = rounded(groupTotal / Math.max(1, numberOfGroupMembers))
+  const averageAmountPerGroupMember = rounded(groupTotal / Math.max(1, groupMembers.length))
 
-  const balances = Object.keys(groupMemberPayments).reduce((acc, name) => {
+  const balances = groupMembers.reduce((acc, name) => {
     acc[name] = rounded(totalPaidPerGroupMember[name] - averageAmountPerGroupMember)
     return acc
   }, {})
 
   const stillOwed = () => entries(balances).filter(([name, balance]) => balance > 0)
   const stillOwing = () => entries(balances).filter(([name, balance]) => balance < 0)
-  const amountOwedByGroupMember = Object.keys(groupMemberPayments).reduce((acc, name) => {
+  const amountOwedByGroupMember = groupMembers.reduce((acc, name) => {
     acc[name] = {}
     return acc
   }, {})
