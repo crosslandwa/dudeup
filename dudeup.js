87c8c9ef8972f0decd152079c1ece58d0e65deea
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-10-02 23:05:35 +0100

    Refactor out calculation of amount owed by each group member

--- dudeup.js
+++ dudeup.js
@@ -1,7 +1,3 @@
-const initial = () => ({
-  amountOwedByGroupMember: {}
-})
-
 const entries = map => Object.keys(map).map(key => [key, map[key]])
 const sum = values => values.reduce((total, x) => rounded(total + x), 0)
 const rounded = amount => Math.round(amount * 1e2) / 1e2
@@ -11,14 +7,7 @@ function DudeUp (groupMemberPayments) {
     acc[name] = sum(groupMemberPayments[name])
     return acc
   }, {})
-
   const groupTotal = sum(entries(totalPaidPerGroupMember).map(([name, total]) => total))
-
-  const result = Object.keys(groupMemberPayments).reduce((acc, name) => {
-    acc.amountOwedByGroupMember[name] = {}
-    return acc
-  }, initial())
-
   const numberOfGroupMembers = Object.keys(groupMemberPayments).length
   const averageAmountPerGroupMember = rounded(groupTotal / Math.max(1, numberOfGroupMembers))
 
@@ -29,13 +18,17 @@ function DudeUp (groupMemberPayments) {
 
   const stillOwed = () => entries(balances).filter(([name, balance]) => balance > 0)
   const stillOwing = () => entries(balances).filter(([name, balance]) => balance < 0)
+  const amountOwedByGroupMember = Object.keys(groupMemberPayments).reduce((acc, name) => {
+    acc[name] = {}
+    return acc
+  }, {})
 
   while (stillOwed().length) {
     const [owedToName, owedToBalance] = stillOwed()[0]
     if (stillOwing().length) {
       const [owedByName, owedByBalance] = stillOwing()[0]
       const amount = Math.min(owedToBalance, Math.abs(owedByBalance))
-      result.amountOwedByGroupMember[owedByName][owedToName] = amount
+      amountOwedByGroupMember[owedByName][owedToName] = amount
       balances[owedToName] = rounded(owedToBalance - amount)
       balances[owedByName] = rounded(owedByBalance + amount)
     } else {
@@ -47,7 +40,7 @@ function DudeUp (groupMemberPayments) {
   return {
     averageAmountPerGroupMember,
     groupTotal,
-    amountOwedByGroupMember: result.amountOwedByGroupMember,
+    amountOwedByGroupMember,
     totalPaidPerGroupMember
   }
 }
