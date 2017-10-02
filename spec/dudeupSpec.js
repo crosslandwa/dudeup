3e6baee0d30c14dd26ff5a8ef504c7d8ab616a0d
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2017-10-02 07:40:52 +0100

    Refactor tests to pass array of amounts instead of map

--- spec/dudeupSpec.js
+++ spec/dudeupSpec.js
@@ -1,83 +1,52 @@
 const DudeUp = require('../dudeup')
 
-const noPayments = () => ({ })
 const compareAsJson = (a, b) => {
   expect(JSON.stringify(a)).toEqual(JSON.stringify(b))
 }
 
 describe('Settle Up', () => {
   it('calculates the total paid per group member', () => {
-    const eAndG = noPayments()
-    const sAndW = noPayments()
-
     compareAsJson(
-      DudeUp({ eAndG, sAndW }).totalPaidPerGroupMember,
+      DudeUp({ eAndG: [], sAndW: [] }).totalPaidPerGroupMember,
       { eAndG: 0, sAndW: 0 }
     )
 
-    eAndG['a'] = 1
-    eAndG['b'] = 2
-    sAndW['c'] = 0.5
-
     compareAsJson(
-      DudeUp({ eAndG, sAndW }).totalPaidPerGroupMember,
+      DudeUp({ eAndG: [1, 2], sAndW: [0.5] }).totalPaidPerGroupMember,
       { eAndG: 3, sAndW: 0.5 }
     )
   })
 
   it('calculates the total paid by the group', () => {
-    const eAndG = noPayments()
-    const sAndW = noPayments()
-    expect(DudeUp({ eAndG, sAndW }).groupTotal).toEqual(0)
-
-    eAndG['a'] = 1
-    eAndG['b'] = 2
-    sAndW['c'] = 0.5
-    expect(DudeUp({ eAndG, sAndW }).groupTotal).toEqual(3.5)
+    expect(DudeUp({ eAndG: [], sAndW: [] }).groupTotal).toEqual(0)
+    expect(DudeUp({ eAndG: [1, 2], sAndW: [0.5] }).groupTotal).toEqual(3.5)
   })
 
   it('calculates the average amount owed per group member', () => {
-    const eAndG = noPayments()
-    const sAndW = noPayments()
-    expect(DudeUp({ eAndG, sAndW }).averageAmountPerGroupMember).toEqual(0)
-
-    eAndG['a'] = 1
-    eAndG['b'] = 2
-    sAndW['c'] = 0.5
-    expect(DudeUp({ eAndG, sAndW }).averageAmountPerGroupMember).toEqual(1.75)
+    expect(DudeUp({ eAndG: [], sAndW: [] }).averageAmountPerGroupMember).toEqual(0)
+    expect(DudeUp({ eAndG: [1, 2], sAndW: [0.5] }).averageAmountPerGroupMember).toEqual(1.75)
   })
 
   it('calculates the amount group members should pay to others (in a group of 2) to settle up', () => {
-    const eAndG = noPayments()
-    const sAndW = noPayments()
     compareAsJson(
-      DudeUp({ eAndG, sAndW }).amountOwedByGroupMember,
+      DudeUp({ eAndG: [], sAndW: [] }).amountOwedByGroupMember,
       { eAndG: {}, sAndW: {} }
     )
 
-    eAndG['a'] = 2
     compareAsJson(
-      DudeUp({ eAndG, sAndW }).amountOwedByGroupMember,
+      DudeUp({ eAndG: [2], sAndW: [] }).amountOwedByGroupMember,
       { eAndG: {}, sAndW: { eAndG: 1 } }
     )
 
-    sAndW['b'] = 3
     compareAsJson(
-      DudeUp({ eAndG, sAndW }).amountOwedByGroupMember,
+      DudeUp({ eAndG: [2], sAndW: [3] }).amountOwedByGroupMember,
       { eAndG: { sAndW: 0.5 }, sAndW: {} }
     )
   })
 
   it('calculates the amount group members should pay to ohers (in a group of 3+) to settle up', () => {
-    const a = noPayments()
-    const b = noPayments()
-    const c = noPayments()
-    const d = noPayments()
-
-    a['aLovelyThing'] = 4
-    b['anotherLovelyThing'] = 2
     compareAsJson(
-      DudeUp({ a, b, c, d }).amountOwedByGroupMember,
+      DudeUp({ a: [4], b: [2], c: [], d: [] }).amountOwedByGroupMember,
       { a: {}, b: {}, c: { a: 1.5 }, d: { a: 1, b: 0.5 } }
     )
   })
