a4d5900952277933e5d9709699b31b9689ec03a3
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-17 21:43:42 +0100

    Create UI elements for Dude List and Add Dude button.
    Fix generation of dude IDs to correctly replace spaces with underscore

--- src/DudeList/__tests__/DudeListTest.js
+++ src/DudeList/__tests__/DudeListTest.js
@@ -5,14 +5,14 @@ describe('Dude List', () => {
   it('can have Dudes added', () => {
     const store = createStore()
 
-    expect(dudeIdsSelector(store.getState()).length).toEqual(0)
+    expect(dudeIdsSelector(store.getState())).toHaveLength(0)
 
     store.dispatch(addDude('person 1'))
     store.dispatch(addDude('person 2'))
 
     const dudeIds = dudeIdsSelector(store.getState())
 
-    expect(dudeIds.length).toEqual(2)
+    expect(dudeIds).toHaveLength(2)
 
     const lastAddedId = dudeIds.slice(-1)
     expect(dudeNameSelector(store.getState(), lastAddedId)).toEqual('person 2')
