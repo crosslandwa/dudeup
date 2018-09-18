995dbcb2217d4f8e6984352f223ceacb28b67bf6
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-18 19:43:09 +0100

    Start adding actions for adding/editing list items

--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import persistState from 'redux-localstorage'
 import { reducer as dudes } from './DudeList/interactions'
+import { reducer as items } from './ItemList/interactions'
 import { reducer as modal } from './Modal/interactions'
 
 const reducer = combineReducers({
   persisted: combineReducers({
-    dudes
+    dudes,
+    items
   }),
   modal
 })
 const naturalEnhancer = (createStore) => (...args) => createStore(...args)
 
-const localStorageAvailable = !!(window && window.localStorage)
+const localStorageAvailable = !!(window && window.localStorage) // TODO window is defined when tests run so state is persisted between store instantiations! (needs fixing in bootstrapper too)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 function createAppStore () {
