1aabd084bdd49cff9167a617473e1207f2cab4a9
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-19 13:14:33 +0100

    Add group total and average per dude

--- src/Summary/__tests__/SummaryTest.js
+++ src/Summary/__tests__/SummaryTest.js
@@ -1,7 +1,7 @@
 import createStore from '../../store'
 import { addDude } from '../../DudeList/interactions'
 import { addItem, updateItemDude, updateItemPrice, itemIdsSelector } from '../../ItemList/interactions'
-import { debtsForDudeSelector, dudesInDebtIdSelector } from '../interactions'
+import { dudesInDebtSummarySelector } from '../interactions'
 
 const addItemAndReturnId = store => {
   store.dispatch(addItem())
@@ -17,7 +17,7 @@ describe('Summary', () => {
     store.dispatch(addDude(dude1))
     store.dispatch(addDude(dude2))
 
-    expect(dudesInDebtIdSelector(store.getState())).toHaveLength(0)
+    expect(dudesInDebtSummarySelector(store.getState()).dudeIds).toHaveLength(0)
   })
 
   it('lists amounts owed when shared items have been bought', () => {
@@ -34,8 +34,42 @@ describe('Summary', () => {
     store.dispatch(updateItemDude(itemId, dude1))
     store.dispatch(updateItemPrice(itemId, 9))
 
-    expect(dudesInDebtIdSelector(store.getState())).toHaveLength(2)
+    expect(dudesInDebtSummarySelector(store.getState()).dudeIds).toHaveLength(2)
 
-    expect(debtsForDudeSelector(store.getState(), dude2)).toEqual([{ dudeId: 'dude1', amount: 3 }])
+    expect(dudesInDebtSummarySelector(store.getState()).debts[dude2]).toEqual([{ dudeId: 'dude1', amount: 3 }])
+  })
+
+  it('gives group total', () => {
+    const store = createStore()
+    const dude1 = 'dude1'
+
+    store.dispatch(addDude(dude1))
+
+    const itemId1 = addItemAndReturnId(store)
+    store.dispatch(updateItemDude(itemId1, dude1))
+    store.dispatch(updateItemPrice(itemId1, 9))
+
+    const itemId2 = addItemAndReturnId(store)
+    store.dispatch(updateItemDude(itemId2, dude1))
+    store.dispatch(updateItemPrice(itemId2, 19))
+
+    expect(dudesInDebtSummarySelector(store.getState()).groupTotal).toEqual(28)
+  })
+
+  it('gives average amount per dude total', () => {
+    const store = createStore()
+    const dude1 = 'dude1'
+    const dude2 = 'dude2'
+    const dude3 = 'dude3'
+
+    store.dispatch(addDude(dude1))
+    store.dispatch(addDude(dude2))
+    store.dispatch(addDude(dude3))
+
+    const itemId = addItemAndReturnId(store)
+    store.dispatch(updateItemDude(itemId, dude1))
+    store.dispatch(updateItemPrice(itemId, 9))
+
+    expect(dudesInDebtSummarySelector(store.getState()).averageAmountPerDude).toEqual(3)
   })
 })
