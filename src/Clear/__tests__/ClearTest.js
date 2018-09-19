b2c215add282eaeb708628d265db1095bc50a838
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-19 16:58:11 +0100

    Add clear functionality to store

---
+++ src/Clear/__tests__/ClearTest.js
@@ -0,0 +1,18 @@
+import createStore from '../../store'
+import { addDude, dudeIdsSelector } from '../../DudeList/interactions'
+import { addItem, itemIdsSelector } from '../../ItemList/interactions'
+import { clear } from '../interactions'
+
+describe('Clear', () => {
+  it('removes all dudes and items', () => {
+    const store = createStore()
+
+    store.dispatch(addDude('person 1'))
+    store.dispatch(addItem())
+
+    store.dispatch(clear())
+
+    expect(dudeIdsSelector(store.getState())).toHaveLength(0)
+    expect(itemIdsSelector(store.getState())).toHaveLength(0)
+  })
+})
