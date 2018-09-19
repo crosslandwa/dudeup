33bdf827afc9ce97c7ae2e6088efda0f7224de43
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-19 16:48:52 +0100

    Remove dude in UI

--- src/DudeManagement/index.js
+++ src/DudeManagement/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { openModal as openAddDudeModal } from '../AddDudeModal/interactions'
+import { openModal as openRemoveDudeModal } from '../RemoveDudeModal/interactions'
 
-const mapDispatchToProps = { openAddDudeModal }
+const mapDispatchToProps = { openAddDudeModal, openRemoveDudeModal }
 
 const DudeManagement = props => (
   <div>
     <input type="button" value="Add Dude" onClick={props.openAddDudeModal} />
+    <input type="button" value="Remove Dude" onClick={props.openRemoveDudeModal} />
   </div>
 )
 
