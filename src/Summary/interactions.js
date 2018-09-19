cea2d99e0f0e8bfbd9a10e474376f0137a78fe26
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-09-19 09:08:53 +0100

    Add owed amounts to summary

--- src/Summary/interactions.js
+++ src/Summary/interactions.js
@@ -5,15 +5,21 @@ import DudeUp from 'dudeup'
 // ------ACTIONS------
 
 // ------SELECTORS------
-export const dudesInDebtIdSelector = state => {
-  const summary = DudeUp(dudeIdsSelector(state)
-    .reduce((acc, dudeId) => ({
-      ...acc,
-      [dudeId]: itemIdsForDudeSelector(state, dudeId).map(itemId => itemPriceSelector(state, itemId))
-    }), {}))
+const calculateSummary = state => DudeUp(dudeIdsSelector(state)
+  .reduce((acc, dudeId) => ({
+    ...acc,
+    [dudeId]: itemIdsForDudeSelector(state, dudeId).map(itemId => itemPriceSelector(state, itemId))
+  }), {}))
 
+export const dudesInDebtIdSelector = state => {
+  const summary = calculateSummary(state)
   return Object.keys(summary.amountOwedByGroupMember)
     .filter(dudeId => Object.keys(summary.amountOwedByGroupMember[dudeId]).length)
+}
+
+export const debtsForDudeSelector = (state, dudeId) => {
+  const owedDudeIdsToAmount = calculateSummary(state).amountOwedByGroupMember[dudeId]
+  return Object.keys(owedDudeIdsToAmount).map(dudeId => ({ dudeId, amount: owedDudeIdsToAmount[dudeId] }))
 }
 
 // ------REDUCERS------
