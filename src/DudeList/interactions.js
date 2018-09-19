4e6c93ae580c866ef5747ac44506bb11d20812d7
will.crossland <will.crossland@digital.hmrc.gov.uk>
2018-09-19 16:27:17 +0100

    Add support to remove dudes from store

--- src/DudeList/interactions.js
+++ src/DudeList/interactions.js
@@ -1,29 +1,12 @@
-import { combineReducers } from 'redux'
-
 // ------ACTIONS------
 export const addDude = (name = 'The other guy') => ({ type: 'DUDELIST_ADD_DUDE', id: name.toLowerCase().replace(/\s+/g, '_'), name })
+export const removeDude = id => ({ type: 'DUDELIST_REMOVE_DUDE', id })
 
 // ------SELECTORS------
 export const dudeIdsSelector = state => state.persisted.dudes.allIds
 export const dudeNameSelector = (state, id) => state.persisted.dudes.byId[id].name
 
 // ------REDUCERS------
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case 'DUDELIST_ADD_DUDE':
-      return state.includes(action.id) ? state : state.concat(action.id)
-  }
-  return state
-}
-
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case 'DUDELIST_ADD_DUDE':
-      return { ...state, [action.id]: dude(state[action.id], action) }
-  }
-  return state
-}
-
 const dude = (state = {}, action) => {
   switch (action.type) {
     case 'DUDELIST_ADD_DUDE':
@@ -32,4 +15,23 @@ const dude = (state = {}, action) => {
   return state
 }
 
-export const reducer = combineReducers({ allIds, byId })
+export const reducer = (state = { allIds: [], byId: {} }, action) => {
+  switch (action.type) {
+    case 'DUDELIST_ADD_DUDE':
+      return {
+        allIds: state.allIds.includes(action.id) ? state.allIds : state.allIds.concat(action.id),
+        byId: {
+          ...state.byId,
+          [action.id]: dude(state[action.id], action)
+        }
+      }
+    case 'DUDELIST_REMOVE_DUDE':
+      const updatedIds = [...state.allIds]
+      updatedIds.splice(state.allIds.indexOf(action.id), 1)
+      return {
+        allIds: updatedIds,
+        byId: { ...state.byId, [action.id]: undefined }
+      }
+  }
+  return state
+}
