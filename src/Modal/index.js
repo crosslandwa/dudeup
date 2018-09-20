01281d1a24a65c1f6791d37b77a33ef8b8aeba86
will.crossland <will.crossland@digital.hmrc.gov.uk>
2018-09-20 09:05:04 +0100

    Close modal with escape key

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -4,36 +4,58 @@ import { closeModal } from './interactions'
 
 const mapDispatchToProps = (dispatch, { onSubmit }) => ({
   closeModal: () => dispatch(closeModal()),
-  onSubmit: e => {
+  onSubmit: event => {
     event.preventDefault()
     onSubmit()
   }
 })
 
-const Modal = props => (
-  <div style={{
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#80808080',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute'
-  }}>
-    <div style={{
-      width: '50vw',
-      height: '50vw',
-      backgroundColor: 'white'
-    }}>
-      <form onSubmit={props.onSubmit} >
-        {props.children}
-        <div>
-          <input type="button" value="Cancel" onClick={props.closeModal} />
-          <input type="submit" value="OK"/>
+class Modal extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.escFunction = event => {
+      if (event.keyCode === 27) {
+        this.props.closeModal()
+      }
+    }
+  }
+
+  componentDidMount () {
+    document.addEventListener('keydown', this.escFunction, false)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.escFunction, false)
+  }
+
+  render () {
+    return (
+      <div style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#80808080',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute'
+      }}>
+        <div style={{
+          width: '50vw',
+          height: '50vw',
+          backgroundColor: 'white'
+        }}>
+          <form onSubmit={this.props.onSubmit} >
+            {this.props.children}
+            <div>
+              <input type="button" value="Cancel" onClick={this.props.closeModal} />
+              <input type="submit" value="OK"/>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-  </div>
-)
+      </div>
+    )
+  }
+}
 
 export default connect(null, mapDispatchToProps)(Modal)
