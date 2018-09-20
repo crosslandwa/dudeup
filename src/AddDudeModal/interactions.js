8207dd73daea80ef58987934c569fcd7af8decb5
will.crossland <will.crossland@digital.hmrc.gov.uk>
2018-09-20 08:46:15 +0100

    Handle close/submission of modals within Modal

--- src/AddDudeModal/interactions.js
+++ src/AddDudeModal/interactions.js
@@ -1,14 +1,28 @@
 import {
-  closeModal as closeModalGeneric,
+  closeModal,
   openModal as openModalGeneric,
   isModalOpenSelector as isModalOpenGeneric
 } from '../Modal/interactions'
+import { addDude } from '../DudeList/interactions'
 
 export const ADD_DUDE_MODAL_ID = 'add-dude-modal'
 
 // ------ACTIONS------
 export const openModal = () => openModalGeneric(ADD_DUDE_MODAL_ID)
-export const closeModal = closeModalGeneric
+export const modalAddDude = name => ({ type: 'ADD_DUDE_MODAL_ADD_DUDE', name })
 
 // ------SELECTORS------
 export const isModalOpenSelector = state => isModalOpenGeneric(state, ADD_DUDE_MODAL_ID)
+
+// ------MIDDLEWARE------
+export function middleware (store) {
+  return (next) => (action) => {
+    switch (action.type) {
+      case 'ADD_DUDE_MODAL_ADD_DUDE':
+        next(addDude(action.name))
+        next(closeModal())
+        return
+    }
+    next(action)
+  }
+}
--- src/RemoveDudeModal/interactions.js
+++ src/RemoveDudeModal/interactions.js
@@ -1,14 +1,28 @@
 import {
-  closeModal as closeModalGeneric,
+  closeModal,
   openModal as openModalGeneric,
   isModalOpenSelector as isModalOpenGeneric
 } from '../Modal/interactions'
+import { removeDude } from '../DudeList/interactions'
 
 export const REMOVE_DUDE_MODAL_ID = 'remove-dude-modal'
 
 // ------ACTIONS------
 export const openModal = () => openModalGeneric(REMOVE_DUDE_MODAL_ID)
-export const closeModal = closeModalGeneric
+export const modalRemoveDude = id => ({ type: 'REMOVE_DUDE_MODAL_REMOVE_DUDE', id })
 
 // ------SELECTORS------
 export const isModalOpenSelector = state => isModalOpenGeneric(state, REMOVE_DUDE_MODAL_ID)
+
+// ------MIDDLEWARE------
+export function middleware (store) {
+  return (next) => (action) => {
+    switch (action.type) {
+      case 'REMOVE_DUDE_MODAL_REMOVE_DUDE':
+        action.id && next(removeDude(action.id))
+        next(closeModal())
+        return
+    }
+    next(action)
+  }
+}
