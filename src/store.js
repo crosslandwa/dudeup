2f858d58ffa2cc46fec3fa5abb276a6aa88d560d
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-10-12 10:39:50 +0100

    Remove remaining use of redux for handling modal

--- src/store.js
+++ src/store.js
@@ -2,15 +2,13 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import persistState from 'redux-localstorage'
 import { reducer as dudes } from './DudeList/interactions'
 import { reducer as items, middleware as itemsMiddleware } from './ItemList/interactions'
-import { reducer as modal } from './Modal/interactions'
 import { resetingReducer } from './Clear/interactions'
 
 const reducer = resetingReducer(combineReducers({
   persisted: combineReducers({
     dudes,
     items
-  }),
-  modal
+  })
 }))
 
 const naturalEnhancer = (createStore) => (...args) => createStore(...args)
