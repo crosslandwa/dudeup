4f44a4cf9354244ad8cbc7ec9a2d000bd0c51484
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-10-15 08:35:42 +0100

    Remove duplication of DudeList

--- src/DudeList/index.js
+++ src/DudeList/index.js
@@ -7,10 +7,12 @@ const mapStateToProps = state => ({
   dudes: dudeIdsSelector(state).map(id => ({ id, name: dudeNameSelector(state, id) }))
 })
 
-const DudeList = ({ dudes, onChange, selectedId = '' }) => (
+const DudeList = ({ customActions = [], dudes, onChange, selectedId = '' }) => (
   <select style={dropdownStyle} value={selectedId} autoFocus onChange={onChange}>
     <option value={''} disabled >Select a dude...</option>
     {dudes.map(({ id, name }) => <option value={id}>{name}</option>)}
+    {customActions.length && <option disabled>───────</option>}
+    {customActions.map(({ id, label }) => <option value={id} >{label}</option>)}
   </select>
 )
 
