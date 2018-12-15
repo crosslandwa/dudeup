d4df8ad55c453780a8eb58c1e65f7677885b4d05
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-12-15 11:03:34 +0000

    Remove dudes from within Update Dude accordian

--- src/DudeList/EditableDude.js
+++ src/DudeList/EditableDude.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { dudeNameSelector, updateDudeName } from './interactions'
+import { dudeCanBeRemovedSelector, dudeNameSelector, removeDude, updateDudeName } from './interactions'
 import { textButtonStyle, textInputStyle } from '../styles'
 import Accordian from '../Accordian'
 import { WithFlyoutArrowBelow } from '../Accordian/FlyoutArrow'
 
 const mapStateToProps = (state, { id }) => ({
-  name: dudeNameSelector(state, id)
+  name: dudeNameSelector(state, id),
+  isRemovable: dudeCanBeRemovedSelector(state, id)
 })
 
 class DudeManagement extends React.Component {
@@ -30,6 +31,11 @@ class DudeManagement extends React.Component {
       this.props.updateDudeName(this.props.id, this.state.name)
       this.closeEdit()
     }
+
+    this.remove = e => {
+      this.props.removeDude(this.props.id)
+      this.closeEdit()
+    }
   }
 
   render () {
@@ -37,10 +43,10 @@ class DudeManagement extends React.Component {
     return (
       <div style={{ margin: '0 1em 0.25em 0' }}>
         <WithFlyoutArrowBelow show={edit} >
-          <input type="button" style={{ ...textButtonStyle }} onClick={this.toggleEditOpen} value={this.props.name} />
+          <input type="button" style={textButtonStyle} onClick={this.toggleEditOpen} value={this.props.name} />
         </WithFlyoutArrowBelow>
         {this.state.editOpen && (
-          <Accordian onCancel={this.closeEdit} onSubmit={this.updateName} submitButtonText="Save" title={`Edit ${this.props.name}`}>
+          <Accordian onCancel={this.closeEdit} onSubmit={this.updateName} submitButtonText="Save" title={`Update ${this.props.name}`}>
             <div style={{
               display: 'flex',
               flexDirection: 'column'
@@ -50,6 +56,15 @@ class DudeManagement extends React.Component {
                 <input style={textInputStyle} value={this.state.name} autoFocus type="textbox" onChange={this.handleTextInput}/>
               </label>
             </div>
+            {this.props.isRemovable
+              ? <input type="button" style={textButtonStyle} onClick={this.remove} value="Remove" />
+              : (
+                <React.Fragment>
+                  <input type="button" style={textButtonStyle} disabled="true" value="Remove" />
+                  <div><em>Dudes who have bought (or are sharing) items can not be removed</em></div>
+                </React.Fragment>
+              )
+            }
           </Accordian>
         )}
       </div>
@@ -57,4 +72,4 @@ class DudeManagement extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, { updateDudeName })(DudeManagement)
+export default connect(mapStateToProps, { removeDude, updateDudeName })(DudeManagement)
--- src/ItemList/index.js
+++ src/ItemList/index.js
@@ -3,39 +3,18 @@ import { connect } from 'react-redux'
 import { addItem, itemIdsSelector } from './interactions'
 import Item from './Item'
 import { textButtonStyle } from '../styles'
-import RemoveDudeAccordian from '../DudeList/RemoveDudeAccordian'
-import { WithFlyoutArrowBelow } from '../Accordian/FlyoutArrow'
 
 const mapStateToProps = state => ({
   ids: itemIdsSelector(state)
 })
 
-class ItemList extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { removeDudeOpen: false }
-    this.closeModal = () => {
-      this.setState({ removeDudeOpen: false })
-    }
-    this.openModal = () => {
-      this.setState({ removeDudeOpen: true })
-    }
-  }
-
-  render () {
-    return (
-      <div>
-        <div>
-          {this.props.ids.map(id => <Item id={id} />)}
-        </div>
-        <input style={{ ...textButtonStyle, display: 'block' }} type="button" onClick={this.props.addItem} value="Add item" />
-        <WithFlyoutArrowBelow show={this.state.removeDudeOpen}>
-          <input style={{ ...textButtonStyle }} type="button" value="Remove dude" onClick={this.openModal} />
-        </WithFlyoutArrowBelow>
-        {this.state.removeDudeOpen && <RemoveDudeAccordian close={this.closeModal} />}
-      </div>
-    )
-  }
-}
+const ItemList = props => (
+  <div>
+    <div>
+      {props.ids.map(id => <Item id={id} />)}
+    </div>
+    <input style={{ ...textButtonStyle, display: 'block' }} type="button" onClick={props.addItem} value="Add item" />
+  </div>
+)
 
 export default connect(mapStateToProps, { addItem })(ItemList)
