a3ebf613deb49ed29cf044925beca41d69fffb99
Will Crossland <4037089+crosslandwa@users.noreply.github.com>
2018-12-19 23:07:45 +0000

    Highlight accordian closing cross on hover

--- src/Accordian/ClosingCross.js
+++ src/Accordian/ClosingCross.js
@@ -1,17 +1,51 @@
 import React from 'react'
 
-const ClosingCross = props => (
-  <div style={{ display: 'flex', justifyContent: 'flex-end', height: 0 }}>
-    <div
-      {...props}
-      style={{
-        cursor: 'pointer',
-        display: 'contents'
-      }}
-    >
-      ✕
-    </div>
-  </div>
-)
+class ClosingCross extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      hover: false
+    }
+
+    this.onHover = () => {
+      this.setState({ hover: true })
+    }
+
+    this.endHover = () => {
+      this.setState({ hover: false })
+    }
+  }
+
+  render () {
+    return (
+      <div
+        onMouseEnter={this.onHover}
+        onMouseLeave={this.endHover}
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          height: '1.5em',
+          marginBottom: '-1.4em'
+        }}
+        {...this.props}
+      >
+        <div
+          style={{
+            cursor: 'pointer',
+            textAlign: 'center',
+            background: this.state.hover ? 'white' : '',
+            borderRadius: '50%',
+            height: '1.5em',
+            width: '1.5em',
+            lineHeight: '1.5em'
+          }}
+        >
+          ✕
+        </div>
+      </div>
+    )
+  }
+}
 
 export default ClosingCross
