ddc5ab775c9f7b55c86e855e74323bbbd1e9366d
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-02-22 00:14:14 +0000

    Adjust header styling - make it full width with no borders (only box-shadow)

--- src/GenericUi/Header.js
+++ src/GenericUi/Header.js
@@ -4,15 +4,10 @@ import { highlightColor } from '../styles'
 const Header = props => (
   <header style={{
     fontFamily: 'helvetica',
-    fontSize: '1em',
     boxShadow: `0 0 1em 0.5em ${highlightColor}`,
-    maxWidth: 800,
-    margin: '0.5em auto 0 auto'
+    margin: '0 auto'
   }}>
     <div style={{
-      margin: '0 auto',
-      maxWidth: 800,
-      border: '1px solid #808080',
       padding: '0.25em',
       textAlign: 'center'
     }}>
