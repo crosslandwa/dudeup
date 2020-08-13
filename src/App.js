3fcb68dad302b857a37f2d5c4c042638d3bcdaa7
will.crossland <will.crossland01@bbc.co.uk>
2020-08-13 10:31:58 +0100

    Add overscroll-behaviour: none to body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import ItemList from './ItemList'
 import Notifications from './Notifications'
 import Summary from './Summary'
@@ -11,31 +10,20 @@ import About from './About'
 import Clear from './Clear'
 import './app.css'
 
-const mapStateToProps = state => ({})
-const mapDispatchToProps = {}
+const App = () => (
+  <>
+    <Header />
+    <div class="du-content-container">
+      <About />
+      <AnErrorOccurred>
+        <Notifications />
+        <Section title="Dudes"><DudeManagement /></Section>
+        <Section title="Items"><ItemList /></Section>
+        <Section title="Settle up..."><Summary /></Section>
+        <Clear />
+      </AnErrorOccurred>
+    </div>
+  </>
+)
 
-class App extends React.Component {
-  componentWillMount () {
-    document.body.style.margin = 0
-  }
-
-  render () {
-    return (
-      <>
-        <Header />
-        <div class="du-content-container">
-          <About />
-          <AnErrorOccurred>
-            <Notifications />
-            <Section title="Dudes"><DudeManagement /></Section>
-            <Section title="Items"><ItemList /></Section>
-            <Section title="Settle up..."><Summary /></Section>
-            <Clear />
-          </AnErrorOccurred>
-        </div>
-      </>
-    )
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
