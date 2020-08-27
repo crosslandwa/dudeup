aeb6fec95c522ba32d5e01794a394278086387ef
will.crossland <will.crossland01@bbc.co.uk>
2020-08-27 11:50:28 +0100

    Allow placeholder value to be specified

--- src/GenericUi/PriceInput.js
+++ src/GenericUi/PriceInput.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { textInputStyle } from '../styles'
 
-const PriceInput = ({ onChange, price, style }) => (
+const PriceInput = ({ onChange, placeholder = 0, price, style }) => (
   <input
     style={{ ...textInputStyle, ...style }}
     type="number"
     step="0.01"
     onChange={onChange}
-    placeholder="0"
+    placeholder={placeholder}
     value={price !== 0 ? price : ''}
   />
 )
